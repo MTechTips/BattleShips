06a4407946a3c8e9d80a3e4586c6ab7058116997
Miles Tech Tips <68609774+MTechTips@users.noreply.github.com>
2024-01-12 16:16:26 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# This will be used to link multiple python files together
+import matty #add what your coding (desc)
+import theo #add what your coding (desc)
+import eleanor #add what your coding (desc)
+import techtips #link multiple pc's for multiplayer
+
