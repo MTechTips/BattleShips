4b66bd3852320ec9c31e397b828727aeaa38716e
m-henniker <95105799+m-henniker@users.noreply.github.com>
2024-01-17 15:01:30 +0000

    Update Shooting.py

--- Shooting.py
+++ Shooting.py
@@ -1,10 +1,18 @@
-import sqlite3
-from tkinter import *
+#import sqlite3
+#from tkinter import *
 
-def shoot(ships,position):
+occupied = [[39,False], [1,True]]
+
+def shoot(occupied):
     global button
-    if button 
-    
-    
+    button_val = input("Move ")
+    button_val == occupied.index
+    if button_val == occupied:
+        print("hit")
+        #return X
+    else:
+        print("miss")
+        #return O
+shoot(occupied)    
 ## miss = databse value false
 ## hit = databse value true
